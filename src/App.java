bccfb2f8d48232cb0961a1a996b347a760af8648
Leticia Beatriz <leticiabeatriz1508@gmail.com>
2024-11-28 11:26:29 -0300

    Adicionando o botão de next

--- src/App.java
+++ src/App.java
@@ -15,55 +15,78 @@ public class App {
           musica1.setnomeMusica("Count-Em");
           musica1.setGenero("Rock");
           musica1.setDuracao(180);
-          musica1.setArquivoAudio("./assets/Brandon-Lake_Count-Em.wav");
+          musica1.setArquivoAudio("Basic-Music-App/aula-de-poo-playmusic/assets/Brandon-Lake_Count-Em.wav");
+
 
           Musica musica2 = new Musica();
-          musica2.setnomeMusica("Tear-Off-The-Roof");
+          musica2.setnomeMusica("That's who i praise");
           musica2.setGenero("Rock");
           musica2.setDuracao(180);
-          musica2.setArquivoAudio("./assets/Brandon-Lake_Tear-Off-The-Roof.wav");
+          musica2.setArquivoAudio("Basic-Music-App/aula-de-poo-playmusic/assets/Brandon-Lake_Thats-Who-I-Praise.wav");
+
+          Musica musica3 = new Musica();
+          musica3.setnomeMusica("Tear-Off-The-Roof");
+          musica3.setGenero("Rock");
+          musica3.setDuracao(180);
+          musica3.setArquivoAudio("Basic-Music-App/aula-de-poo-playmusic/assets/Brandon-Lake_Tear-Off-The-Roof.wav");
 
           Album album1 = new Album();
           album1.setNomeAlbum("Primeiro artista");
           album1.setAno(2023);
           album1.addMusica(musica1);
+          album1.addMusica(musica2);
+          album1.addMusica(musica3);
           
           Artista BrandonLake = new Artista();
           BrandonLake.setNome("Brandon Lake");
           BrandonLake.addAlbum(album1);
 
           System.out.println("Abrindo PlayMusic...");
-          BrandonLake.getAlbuns().get(0).getMusicas().get(0).getArquivoAudio();
 
           AudioPlayer player = new AudioPlayer();
-          player.loadAudio(BrandonLake.getAlbuns().get(0).getMusicas().get(0).getArquivoAudio());
 
            // Cria o botão Play/Stop e configura sua ação
           JButton playStopButton = new JButton("Play");
+          
+          player.listMusic(BrandonLake.getAlbuns().get(0).getMusicas());
+
           playStopButton.addActionListener(new ActionListener() {
                @Override
                public void actionPerformed(ActionEvent e) {
                     if (!player.isPlaying) {
                          player.playAudio();
                          playStopButton.setText("Stop");
-                    } else {
+                    }else{
                          player.stopAudio();
                          playStopButton.setText("Play");
                     }
                }
           });
 
-          ImageIcon icon = new ImageIcon("./assets/imgMusic.png");
+          JButton nextAudioButton = new JButton("Next Audio");
+          nextAudioButton.addActionListener(new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+                    if(player.isPlaying){
+                         player.nextAudio();
+                    }else{
+                         player.nextAudio();
+                         playStopButton.setText("Stop");
+                    }
+               }
+          });
+
+          ImageIcon icon = new ImageIcon("Basic-Music-App/aula-de-poo-playmusic/assets/imgMusic.png");
           //Exibe um JOptionPane com o botão Play/Stop
           JOptionPane.showOptionDialog(
                 null,
-                BrandonLake.getAlbuns().get(0).getMusicas().get(0).getnomeMusica(),
+                player,
                 "PlayMusic",
                 JOptionPane.DEFAULT_OPTION,
                 JOptionPane.PLAIN_MESSAGE,
                 icon,
-                new Object[] { playStopButton }, playStopButton);
-          
+                new Object[] { playStopButton, nextAudioButton }, playStopButton);
+
           // Fecha o clip de áudio ao encerrar o programa
           if (player.audioClip != null) {
                player.audioClip.close();
--- src/AudioPlayer.java
+++ src/AudioPlayer.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -9,14 +10,25 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import dominio.Musica;
 
 public class AudioPlayer {
+    private List<Musica> listaDeReproducao;
+    private int indice;
     public Clip audioClip;
     public boolean isPlaying = false;
+
   
     public void loadAudio(String filePath) {
       System.out.println("loadAudio: " + filePath);
       try {
+
+        // Se um áudio já estiver carregado, pare e libere o recurso
+        if (audioClip != null && audioClip.isOpen()) {
+          audioClip.stop();
+          audioClip.close();
+        }
+
         // Carrega o arquivo de áudio
         File audioFile = new File(filePath);
         AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
@@ -38,6 +50,12 @@ public class AudioPlayer {
         e.printStackTrace();
       }
     }
+
+    public void listMusic(List<Musica> musicas){
+      this.listaDeReproducao = musicas;
+      this.indice = 0;
+      loadAudio(musicas.get(indice).getArquivoAudio());
+    }
   
     public void playAudio() {
       System.out.println("playAudio");
@@ -55,6 +73,22 @@ public class AudioPlayer {
         System.out.println("stopAudio stop");
         audioClip.stop();
         isPlaying = false;
+      }
+    }
+
+    public void nextAudio (){
+      System.out.println("nextAudio");
+      stopAudio();
+      if(this.indice < listaDeReproducao.size() - 1){
+        indice ++;
+        loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
+        playAudio();
+      }else if(this.indice == listaDeReproducao.size() - 1){
+        indice = 0;
+        loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
+        playAudio();
+      }else{
+        System.out.println("Sua lista de reprodução está vazia!\nAdicione músicas para tocar...");
       }
     }
 
